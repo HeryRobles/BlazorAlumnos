8f57cd2f320f7cc01a68320f7ea170941ed09900
Heriberto Robles Coronado <rrobleshc@hotmail.com>
2022-11-18 20:33:06 -0700

    Final del proyecto

--- BlazorAlumnos/Server/Model/Entities/Materia.cs
+++ BlazorAlumnos/Server/Model/Entities/Materia.cs
@@ -3,8 +3,8 @@
     public class Materia
     {
         public int Id { get; set; }
-        public string Nombre { get; set; }  
+        public string Nombre { get; set; }
 
-        
+
     }
 }
